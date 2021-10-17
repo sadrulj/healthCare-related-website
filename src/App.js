4d056c8e5d5e23af8c52f0393cc1904f3a09ec23
sadrulj <jamil.sadrul@gmail.com>
2021-10-18 01:08:07 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 class="text-8xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate mt-16">
+        Welcome to HealthClub
+      </h1>
+    </div>
+  );
+}
+
+export default App;
